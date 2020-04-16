ede5116076b614347d5d554486eed8607eda98c6
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2020-04-16 14:57:14 +0200

    Mock Oracle queries and mock workflow api

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 let express = require('express'),
     compression = require('compression'),
-    products = require('./server/products'),
-    database = require('./server/database.js'),
+    // products = require('./server/products'),
+    // database = require('./server/database.js'),
     app = express();
 
   try {
     console.log('Initializing database module');
-    database.initialize(); 
+    // database.initialize();
   } catch (err) {
     console.error(err); 
     process.exit(1); // Non-zero failure code
@@ -37,8 +37,54 @@ app.all('*', function (req, res, next) {
     }
 });
 
-app.get('/products', products.findAll);
-app.get('/product/:lid', products.findByProdId);
+// app.get('/products', products.findAll);
+// app.get('/product/:lid', products.findByProdId);
+app.get('/products', function(req,res) {
+  const products = [{
+    NR_SUBBANDS: 123, OBSERVATIONID: 'obsid1', LID: 'lid1', DECLINATION: 32.1, RIGHTASCENSION: 56.7, STARTTIME: new Date(2020,4,4), ENDTIME: new Date(2020,4,10)
+  }, {
+    NR_SUBBANDS: 123, OBSERVATIONID: 'obsid2', LID: 'lid2', DECLINATION: 32.1, RIGHTASCENSION: 56.7, STARTTIME: new Date(2020,4,4), ENDTIME: new Date(2020,4,10)
+  }, {
+    NR_SUBBANDS: 123, OBSERVATIONID: 'obsid3', LID: 'lid3', DECLINATION: 32.1, RIGHTASCENSION: 56.7, STARTTIME: new Date(2020,4,4), ENDTIME: new Date(2020,4,10)
+  }]
+  res.json({products});
+});
+app.get('/product/:lid', function(res, req) {
+  const products = [{
+    URI: 'https://someuri/' + req.params.lid
+  }];
+  res.json({"products": products});
+});
+app.get('/pipelineschemas', function(req,res) {
+  const pipelineschemas = {
+    pipeline1: {
+      label: 'Pipeline 1',
+      id: 'pipeline1',
+      schema: {
+          "type": "object",
+          "title": "Configuration Parameters:",
+          "description": "This is the LOFAR GRID Pre-Processing Pipeline. Here we print a description of the pipeline.",
+          "properties": {
+            "avg_freq_step": {
+              "type": "integer",
+              "title": "avg_freq_step",
+              "description": "corresponds to .freqstep in NDPPP .type=average , or in case of .type=demixer it is the demixer.freqstep",
+              "default": 2,
+              "minimum": 0,
+              "exclusiveMinimum": true,
+              "maximum": 1000,
+              "exclusiveMaximum": true,
+              "propertyOrder": 1
+            },
+          },
+          "required": [
+              "avg_freq_step",
+          ]
+      }
+    }
+  };
+  res.json({pipelineschemas});
+});
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
